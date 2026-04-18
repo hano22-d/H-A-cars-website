af8fa224b12b081ecfd59c4ab67ce62d776c0b69
hani joma <jomahani41@gmail.com>
2026-04-18 19:43:28 +0300

    rapairs

--- src/components/UiComponents/galleryShowGrid.jsx
+++ src/components/UiComponents/galleryShowGrid.jsx
@@ -3,12 +3,12 @@ import CardGallery from "./cardGallery";
 
 function ShowGrid({ datas }) {
   return (
-    <Grid container mt={{lg: 5,xs:3}} pl={{lg:3,xs: 0}} rowSpacing={{lg:2,xs: 0}} columnSpacing={{xs: 1}}>
+    <Grid container mt={{lg: 5,xs:3}} pl={{lg:3,xs: 0}} rowSpacing={{lg:1,xs: 0}} columnSpacing={{xs: 1}}>
     {datas.flatMap((data) =>
       Object.values(data.mainImages).map((img, index) => (
         <Grid item lg={3} xs={6} key={data.id + "-" + index}>
           <CardGallery
-            height={200}
+            height={175}
             src={img}
             name={`${data.brand} ${data.model}`}
             id={data.id}
