532022ec75eea7a0cb768f142143363ef62eb889
hani joma <jomahani41@gmail.com>
2026-04-18 19:55:34 +0300

    rapairs

--- src/components/UiComponents/cardGallery.jsx
+++ src/components/UiComponents/cardGallery.jsx
@@ -13,72 +13,72 @@ function CardGallery({ src, name, height, id }) {
   const navigate = useNavigate();
 
   return (
-    <Box
-      overflow={"hidden"}
-      sx={{
-        width: { lg: 250, xs: "100%" },
-        height: height,
-        aspectRatio: "4/3",
-        borderRadius: { lg: 2, xs: 1 },
-        mb: "15px",
-      }}
-    >
-      {!loaded && <GallerySkeleton height={height} />}
-      <Card
-        onMouseEnter={() => setShowDetails(true)}
-        onMouseLeave={() => setShowDetails(false)}
-        onClick={() => setShowDetails((prev) => !prev)}
-        className={showdetails ? "bluringCard" : ""}
-        component={"img"}
-        onLoad={() => setLoaded(true)}
-        src={src}
-        loading="lazy"
+      <Box
+        overflow={"hidden"}
         sx={{
-          width: "100%",
-          height: "100%",
-          opacity: loaded ? 1 : 0,
-          transition: "all 0.4s ease",
-        }}
-      />
-      <Stack
-        onMouseEnter={() => setShowDetails(true)}
-        onMouseLeave={() => setShowDetails(false)}
-        onClick={() => setShowDetails((prev) => !prev)}
-        className={showdetails ? "showDetailsGaleery" : ""}
-        sx={{
-          height: { lg: "20%", xs: "70%" },
-          justifyContent: "center",
-          alignItems: "center",
-          opacity: 0,
-          visibility: "hidden",
-          transform: "translateY(0px)",
-          transition: "all 0.4s ease",
+          width: { lg: 250, xs: "100%" },
+          height: height,
+          aspectRatio: "4/3",
+          borderRadius: { lg: 2, xs: 1 },
+          mb: "15px",
         }}
       >
-        <Typography
-          variant="h6"
-          textAlign={"center"}
-          sx={{ color: "#fff", fontSize: { xs: 15, lg: 20 } }}
-        >
-          {name}
-        </Typography>
-        <Button
-          onClick={() => navigate(`/gallery/car/${id}`)}
-          variant={"outlined"}
+        {!loaded && <GallerySkeleton height={height} />}
+        <Card
+          onMouseEnter={() => setShowDetails(true)}
+          onMouseLeave={() => setShowDetails(false)}
+          onClick={() => setShowDetails(prev => !prev)}
+          className={showdetails ? "bluringCard" : ""}
+          component={"img"}
+          onLoad={() => setLoaded(true)}
+          src={src}
+          loading="lazy"
           sx={{
-            backgroundColor: { xs: "#ff4c29", lg: "transparent" },
-            color: { xs: "#fff", lg: "#ff4c29" },
-            width: { lg: "60%", xs: "90%" },
-            border: { lg: "1px solid #ff4c29" },
-            fontSize: { xs: 15, lg: 17 },
-            borderRadius: { xs: 0.5, lg: 1 },
-            p: { xs: 0.5, lg: 1 },
+            width: "100%",
+            height: "100%",
+            opacity: loaded ? 1 : 0,
+            transition: "all 0.4s ease",
+          }}
+        />
+        <Stack
+          onMouseEnter={() => setShowDetails(true)}
+          onMouseLeave={() => setShowDetails(false)}
+          onClick={() => setShowDetails(prev => !prev)}
+          className={showdetails ? "showDetailsGaleery" : ""}
+          sx={{
+            height: {lg:"20%",xs: "70%"},
+            justifyContent: "center",
+            alignItems: "center",
+            opacity: 0,
+            visibility: "hidden",
+            transform: "translateY(-50px)",
+            transition: "all 0.4s ease",
           }}
         >
-          View Details
-        </Button>
-      </Stack>
-    </Box>
+          <Typography
+            variant="h6"
+            textAlign={"center"}
+            sx={{ color: "#fff", fontSize: { xs: 15, lg: 20 } }}
+          >
+            {name}
+          </Typography>
+          <Button
+            onClick={() => navigate(`/gallery/car/${id}`)}
+            variant={'outlined'}
+            sx={{
+              backgroundColor: { xs: "#ff4c29", lg: "transparent" },
+              color: { xs: "#fff", lg: "#ff4c29" },
+              width: { lg: "60%", xs: "90%" },
+              border: { lg: "1px solid #ff4c29" },
+              fontSize: { xs: 15, lg: 17 },
+              borderRadius: {xs:0.5,lg: 1},
+              p: {xs:0.5,lg: 1}
+            }}
+          >
+            View Details
+          </Button>
+        </Stack>
+      </Box>
   );
 }
 export default CardGallery;
